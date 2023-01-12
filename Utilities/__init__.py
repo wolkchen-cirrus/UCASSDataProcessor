07d4b312bd6045c7aa050f10992a8ecd5d4a60b6
J. Girdwood <jg17acv@herts.ac.uk>
2023-01-12 17:33:25 +0000

    Added docstring

--- Utilities/__init__.py
+++ Utilities/__init__.py
@@ -1,15 +1,46 @@
 import pandas as pd
 import datetime as dt
+import os
 
 
-def csv_log_timedelta(filepath, hours, time_format_str, out_time_format_str, time_header, names, header=0):
+def csv_log_timedelta(filepath, hours, time_format_str, out_time_format_str, time_header, names,
+                      change_fn=False, header=0):
+    """
+    a function to add or subtract a number of hours from a log file
+    ...
+
+    Parameters:
+    -----------
+    :param filepath - absolute file path
+    :param hours - integer number of hours to add or subtract
+    :param time_format_str - input datetime format string
+    :param out_time_format_str - output datetime format string
+    :param time_header - string, which header specified in 'names' is the time column
+    :param names - column names for data frame
+    :param change_fn - change filename label? boolean true or false
+    :param header - which row are the headers on, 0 if no headers
+    ...
+
+    Returns:
+    --------
+    :returns - the old and new (if changed) filepaths as a tuple
+    """
+    # Change time in file column
     df = pd.read_csv(filepath, delimiter=',', header=header, names=names).dropna()
     df[time_header] = pd.to_datetime(df[time_header], format=time_format_str) + dt.timedelta(hours=hours)
     df[time_header] = df[time_header].dt.strftime(out_time_format_str)
-    new_start_time = pd.to_datetime('_'.join([filepath.split('\\')[-1].split('_')[-3], filepath.split('\\')[-1].split('_')[-2]]), format='%Y%m%d_%H%M%S%f')
-    filepath = '\\'.join(['\\'.join(filepath.split('\\')[:-2]), '_'.join([filepath.split('\\')[-1].split('_')[:-2], new_start_time, filepath.split('\\')[-1].split('_')[-1]])])
+    # Change time in filename
+    old_filepath = filepath
+    if change_fn is True:
+        new_start_time = (pd.to_datetime('_'.join([os.path.split(filepath)[-1].split('_')[-3],
+                                                   os.path.split(filepath)[-1].split('_')[-2]]), format='%Y%m%d_%H%M%S%f') +
+                          dt.timedelta(hours=hours)).strftime('%Y%m%d_%H%M%S')+'00'
+        path_list = [os.path.split(filepath)[0], '_'.join(['_'.join(os.path.split(filepath)[-1].split('_')[:-3]),
+                                                           new_start_time, os.path.split(filepath)[-1].split('_')[-1]])]
+        filepath = os.path.join(*path_list)
+    # Save new file
     if header == 0:
-        pass
+        df.to_csv(path_or_buf=filepath, index=False, header=False)
     else:
-        pass
-    return df
+        df.to_csv(path_or_buf=filepath)
+    return old_filepath, filepath
