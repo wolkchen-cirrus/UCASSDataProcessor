c9127876b4ee17f958b5a96c3266c7f99fa6f8ec
jgirdwood <j.girdwood@herts.ac.uk>
2023-02-17 03:32:20 +0000

    debugging

--- UCASSData/ArchiveHandler/HDF5DataObjects/CampaignFile.py
+++ UCASSData/ArchiveHandler/HDF5DataObjects/CampaignFile.py
@@ -14,74 +14,63 @@ class CampaignFile(object):
                                 -> Dataframe 2 | Metadata 2
                 -> Flight 2     -> Dataframe 1 | Metadata 1
 
+    This is essentially a wrapper for an HDF5 file.
+
+    :param fn: filename
+    :param mode: file open mode
     """
-    def __init__(self, dd: H5dd, fn: str = None, w: bool = False):
+    def __init__(self, fn: str, mode: str = 'r'):
         self.__fn = None
         self.fn = fn
 
-        self.h5ver: str = ch.getval('h5ver')
-        self.dd = dd
-        self.w = w
+        self.__mode = None
+        self.mode = mode
 
+        self.__dd: H5dd
         self.__f = None
 
-        self._file_check()
+        self.h5ver: str = ch.getval('h5ver')
 
-    def __add__(self, other):
-        if isinstance(other, CampaignFile):
-            self.dd.__add__(other.dd)
-        elif isinstance(other, H5dd):
-            self.dd.__add__(other)
-        return self
-
-    def __delitem__(self, key):
-        self.dd.__delitem__(key)
+        print(f'File check returns {bool(self)}')
 
     def __bool__(self):
-        return self._file_check()
+        return self.__file_check()
 
     def __repr__(self):
         return f'{self.fn} with {len(self.__groups())} groups'
 
     def __enter__(self):
-        self.__open_file('rw')
+        self.__f = h5.File(self.fn, self.mode, libver=('earliest', self.h5ver))
         return self.__f
 
     def __exit__(self):
-        self.__close_file()
+        self.__f.close()
 
-    def _file_check(self):
+    def write(self):
+        if bool(self):
+            raise RuntimeError(f"File check is {bool(self)}")
+
+    def read(self):
+        pass
+
+    def __groups(self):
+        return self.__f.keys()
+
+    def __datasets(self):
+        ds = []
+        for gn in self.__groups():
+            pass
+
+    def __file_check(self):
+        """Returns false if file is invalid or does not exist"""
         if not os.path.exists(self.fn):
             return False
-        gns = self.__groups()
-        for gn in gns:
+        for gn in self.__groups():
             try:
                 dt.strptime(gn, ch.getval("groupDTformat"))
             except ValueError:
-                raise AttributeError("1 or more bad HDF5 groups")
-
-    def save(self):
-        if os.path.exists(self.fn):
-            if self.w:
-                print(f"File at {self.fn} exists, overwriting")
-            else:
-                raise FileExistsError
-        else:
-            self.__open_file('w')
-            # TODO: figure this out
-            self.__close_file()
-
-    def __open_file(self, mode):
-        self.__f = h5.File(self.fn, mode, libver=('earliest', self.h5ver))
-
-    def __close_file(self):
-        self.__f.close()
-
-    def __groups(self):
-        self.__open_file('r')
-        k = self.__f.keys()
-        self.__close_file()
-        return k
+                return False
+        return True
 
     @property
     def fn(self):
@@ -91,7 +80,16 @@ class CampaignFile(object):
     def fn(self, val):
         if os.path.isabs(val):
             self.__fn = val
-        elif val is None:
-            print("No file path specified")
         else:
             raise ValueError("must be abs path")
+
+    @property
+    def mode(self):
+        return self.__mode
+
+    @mode.setter
+    def mode(self, val):
+        if val not in ['r', 'r+', 'w', 'w-', 'x', 'a']:
+            raise ValueError
+        else:
+            self.__mode = val
