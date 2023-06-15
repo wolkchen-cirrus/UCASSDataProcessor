eb31539206efcba0f002131c4052f7202116b4b4
JGirdwood <jessgirdwood@protonmail.com>
2023-06-15 11:16:10 +0100

    merge

--- oproc/ProcHandler/ProcObjects/Calibrate.py
+++ oproc/ProcHandler/ProcObjects/Calibrate.py
@@ -1,5 +1,6 @@
 from .__Proc import Proc
 from datetime import datetime as dt
+from ..ProcHandler import ProcLib as pl
 
 
 # Redefining print function with timestamp
@@ -16,11 +17,16 @@ print = timestamped_print
 class Calibrate(Proc):
 
     def __proc(self, **kwargs):
+        try:
+            mat = kwargs["material"]
+        except KeyError:
+            raise ValueError("Material must be assigned to kwargs")
         data = self.di.__dict__()
         if "bbs_sca" in data:
             print("Already calibrated, nothing to do")
             return self.di
         cof = data["cali_coeffs"]
+        mie_curve = pl.get_material_data(mat)
 
     def __repr__(self):
         return "Calibrate"
